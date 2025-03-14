8fc53549152ea45e561195c17938a7044993e4ef
Hendry <hduarteyt@gmail.com>
2025-03-14 14:01:19 -0300

    letting the user register

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    #path("login/", views.user_login, name="login"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
 
@@ -17,4 +17,6 @@ urlpatterns = [
     path("password-reset/complete", auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
     
     path("", views.dashboard, name="dashboard"),
+
+    path("register/", views.register, name="register")
 ]
