b2062274652cf76281b9f8d1529456fc885f16d0
Hendry <hduarteyt@gmail.com>
2025-04-08 10:28:16 -0300

    overriding the save() method of a modelform to save and donwload the image

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('social-auth/', include('social_django.urls'), name="social")
+    path('social-auth/', include('social_django.urls'), name="social"),
+    path('images/', include('images.urls'), name="images")
 ]
 
 #config para servir los archivos multimedia
