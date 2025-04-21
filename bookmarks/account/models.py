d6681b8c46263b64221755a31e7d653f21a13bc2
Hendry <hduarteyt@gmail.com>
2025-04-21 11:35:53 -0300

    Creating an intermediate model for a following system with manyToMany

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 class Profile(models.Model): #extendiendo las funcionalidades del User Model 
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
@@ -10,3 +11,27 @@ class Profile(models.Model): #extendiendo las funcionalidades del User Model
 
     def __str__(self):
         return f"Perfil de {self.user.first_name if self.user.first_name else self.user.username}"
+    
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    
+    created = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        
+        ordering = ['-created']
+        
+    def __str__(self):
+        return f"{self.user_from} sigue a {self.user_to}"
+    
+user_model = get_user_model()
+user_model.add_to_class(
+    'siguiendo',
+    models.ManyToManyField('self',through=Contact, related_name='seguidores', symmetrical=False)
+)
+    
