88ebc31bcf6fd9e2fbee772236f5ac1ae92a7485
Hendry <hduarteyt@gmail.com>
2025-04-23 11:14:16 -0300

    Adding generic relations in my models using contenttypes

---
+++ bookmarks/actions/utils.py
@@ -0,0 +1,6 @@
+from django.contrib.contenttypes.models import ContentType
+from . import models
+
+def create_action(user, verb, target=None):
+    action = models.Action(user=user, verb=verb, target=target)
+    action.save()
