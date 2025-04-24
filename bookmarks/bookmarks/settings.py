9c815d96e9f9e7f705f0cbf96bcc8bbe4d9a6692
Hendry <hduarteyt@gmail.com>
2025-04-24 15:09:26 -0300

    adding redis to the proyect. saving the views of an image's detail in redis

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -64,6 +64,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,9 +4,18 @@ from django.contrib import messages
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse, HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import redis
+from django.conf import settings
 from .forms import ImageCreateForm
 from .models import Image
 from actions.utils import create_action
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 @login_required
 def image_create(request):
@@ -28,8 +37,10 @@ def image_create(request):
 @login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request, "images/image/detail.html", {"section": "images",
-                                                        "image": image})
+                                                        "image": image,
+                                                        'total_views':total_views})
 
 @require_POST
 @login_required
